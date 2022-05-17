1158065a749ad1fd031582d4465dc34ea2e029c4
kbmelo <kbmelo@outlook.com>
2022-05-17 19:01:32 -0300

    feat(confirm-slide-up) :sparkles: finishing style + animation

--- src/views/Pages/Home/Home.tsx
+++ src/views/Pages/Home/Home.tsx
@@ -1,13 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import SlideUpConfirmation from '../../components/SlideUpConfirmation';
+import Button from '../../components/Button';
 import { Container, Logo } from './styles';
 
 const Home: FC = () => {
+  const [shouldShow, setShouldShow] = useState(false);
   return (
     <Container>
       <Logo />
 
-      <SlideUpConfirmation />
+      <Button onPress={() => setShouldShow(!shouldShow)}>
+        Toggle Confirmation
+      </Button>
+
+      <SlideUpConfirmation
+        title="Tem certeza que deseja finalizar a partida?"
+        okText="Sim"
+        cancelText="Cancelar"
+        shouldShow={shouldShow}
+        onCancel={() => setShouldShow(false)}
+      />
     </Container>
   );
 };
--- src/views/components/SlideUpConfirmation/styles.ts
+++ src/views/components/SlideUpConfirmation/styles.ts
@@ -1,17 +1,40 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Animated } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled(View)`
+export const EmptySpace = styled(ScrollView)``;
+
+export const TouchableArea = styled(View)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 99;
+  background-color: ${({ theme }) => theme.colors.grayLayer};
+`;
+
+export const Container = styled(Animated.View)`
   background: ${({ theme }) => theme.colors.light};
+  border-radius: 4px 4px 0 0;
+
   width: 100%;
   padding: 40px;
 
   position: fixed;
   bottom: 0;
+  z-index: 100;
 `;
 
 export const Question = styled(Text)`
   font-size: 18px;
   font-weight: 400;
   text-align: center;
+`;
+
+export const Footer = styled(View)`
+  flex-direction: row;
+  flex-wrap: nowrap;
+  margin-top: 32px;
+`;
+
+export const ButtonWrapper = styled(View)`
+  width: 50%;
 `;
